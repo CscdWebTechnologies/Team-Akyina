f62a15ed145fa63a7b6748b7fd9f594a6dfd882c
unknown <akyina>
2021-05-15 02:47:58 +0000

    worked on the blue banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import BlueBanner from "./components/BlueBanner";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <BlueBanner />
     </div>
   );
 };
